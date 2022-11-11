48880f4a538a85e689796fac619f79288e27045d
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2022-11-12 01:45:49 +0200

    add map
    add custom map and marker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useJsApiLoader } from "@react-google-maps/api";
 import "./App.css";
 import Form from "./components/Form/Form";
+import Map from "./components/Map/Map";
 
 import Layout from "./layouts/Layout";
 
-function App() {
+const App = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: API_KEY,
+  });
   return (
     <Layout>
-      <Form />
+      <div className="main_container ">
+        <Form />
+        {isLoaded ? <Map /> : <h2>Loading...</h2>}
+      </div>
     </Layout>
   );
-}
+};
 
 export default App;
